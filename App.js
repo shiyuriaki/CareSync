35d20b288d367cd31e26e174a807028628e0eca6
shiyuriaki <127173434+shiyuriaki@users.noreply.github.com>
2025-03-23 09:15:28 -0700

    Add files via upload
    Edited CSS of PatientForm

---
+++ App.js
@@ -0,0 +1,45 @@
+// import logo from './logo.svg';
+//import { useState } from 'react';
+//import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import './App.css';
+import PatientForm from "./PatientForm";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Logo from "./components/Logo/Logo";
+import Panels from "./pages/Login/Panels";
+
+function Main() {
+  return (
+    <div className="App">
+      <section id="home">
+       <Navbar />
+        <Logo />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="login">
+        <Login/>
+      </section>
+    </div>
+  );
+}
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/patient-form" element={<PatientForm/>}/>
+        </Routes>
+      </Router>
+  );
+}
+export default App;
